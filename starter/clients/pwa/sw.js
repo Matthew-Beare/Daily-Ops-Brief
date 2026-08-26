0417df7527afd060c578c2f114f4ccca6a091c27
Matthew Beare <Matthew.A.Beare@gmail.com>
2026-08-26 02:05:44 -0600

    Cache Android inset styling with the shared shell

--- starter/clients/pwa/sw.js
+++ starter/clients/pwa/sw.js
@@ -1,10 +1,10 @@
 "use strict";
 
-const CACHE = "mirror-client-shell-v11";
+const CACHE = "mirror-client-shell-v12";
 const SHELL = [
   "./", "./index.html", "./app.js", "./client-hardening.js", "./platform-ui.js", "./product-v1.js",
   "./smart-capture.js", "./guided-migration.js", "./ble-proximity.js", "./receipt-v1.js", "./native-updater.js", "./integrations-v1.js", "./dashboard-v2.js",
-  "./commercial.css", "./sleek-v2.css",
+  "./commercial.css", "./sleek-v2.css", "./android-fixes.css",
   "./manifest.webmanifest", "./icon.svg", "./brand-mark.svg", "./brand-wordmark.svg", "./tutorial-google.svg", "./tutorial-github.svg", "./tutorial-conflict.svg", "./tutorial-hierarchy.svg"
 ];
 
